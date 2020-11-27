5fc0393ae45e380fff420df096573da50015e6c3
Michael Cullen <71766209+MichaelCullen2011@users.noreply.github.com>
2020-11-27 22:41:02 +0000

    SolarSystem.py now works with the sun and earth. Started implementing functionality for the other planets too, but need to generate a dict of speeds at respective perihelions

--- einsteinpy test scripts/GR_Animations.py
+++ einsteinpy test scripts/GR_Animations.py
@@ -14,8 +14,12 @@ from astropy import units as u
 Variables
 '''
 attractor = Body(name="BH", mass=6e24 * u.kg, parent=None)
-sph_obj = SphericalDifferential(130 * u.m, np.pi/2 * u.rad, -np.pi/8 * u.rad,
-                                0 * u.m / u.s, 0 * u.rad / u.s, 1900 * u.rad / u.s)
+sph_obj = SphericalDifferential(130 * u.m,
+                                np.pi/2 * u.rad,
+                                -np.pi/8 * u.rad,
+                                0 * u.m / u.s,
+                                0 * u.rad / u.s,
+                                1900 * u.rad / u.s)
 object = Body(differential=sph_obj, parent=attractor)
 geodesic = Geodesic(body=object, time=0 * u.s, end_lambda=0.002, step_size=5e-8)
 
@@ -24,7 +28,7 @@ geodesic = Geodesic(body=object, time=0 * u.s, end_lambda=0.002, step_size=5e-8)
 Plotting Animation
 '''
 obj = StaticGeodesicPlotter()
-obj.animate(geodesic, interval=25)
+obj.animate(geodesic, interval=10)
 obj.show()
 
 
