9f93a2a2e912c48879f4ba1999bfad5a2b6b3144
Michael Cullen <71766209+MichaelCullen2011@users.noreply.github.com>
2021-01-14 13:12:18 +0000

    cleaned up the loops and dict creations to combine into fewer loops

--- GR_SolarSystem.py
+++ GR_SolarSystem.py
@@ -5,16 +5,16 @@ from einsteinpy.plotting import StaticGeodesicPlotter
 from einsteinpy.coordinates import SphericalDifferential
 from einsteinpy.bodies import Body
 from einsteinpy.geodesic import Geodesic
-from einsteinpy.plotting import GeodesicPlotter
 
 '''
 Defining Variables and Objects
 '''
 grav_constant = 6.67430e-20 * u.km**3 / (u.kg * u.s**2)     # km3 kg-1 s-2
-bodies_list = ['sun', 'mercury', 'venus',
-               'earth', 'moon', 'mars',
-               'jupiter', 'saturn', 'uranus',
-               'neptune']
+bodies_list = ['sun', 'mercury', 'venus', 'earth', 'moon']
+all_bodies_list = ['sun', 'mercury', 'venus',
+                   'earth', 'moon', 'mars',
+                   'jupiter', 'saturn', 'uranus',
+                   'neptune']
 radius = {'sun': 696340 * u.km, 'mercury': 2439.5 * u.km, 'venus': 6052 * u.km,
           'earth': 6371 * u.km, 'moon': 3389.5 * u.km, 'mars': 3389.5 * u.km,
           'jupiter': 142984/2 * u.km, 'saturn': 120536/2 * u.km, 'uranus': 51118/2 * u.km,
@@ -33,37 +33,26 @@ mass = {'sun': 1.989e30 * u.kg, 'mercury': 0.33e24 * u.kg, 'venus': 4.87e24 * u.
         'earth': 5.972e24 * u.kg, 'moon': 7.34767309e22 * u.kg, 'mars': 6.39e23 * u.kg,
         'jupiter': 1898e24 * u.kg, 'saturn': 568e24 * u.kg, 'uranus': 86.8e24 * u.kg,
         'neptune': 103e24 * u.kg}
-theta_deg = {'sun': 0, 'mercury': 1, 'venus': 1,
-             'earth': 1, 'moon': 1, 'mars': 1,
-             'jupiter': 1, 'saturn': 1, 'uranus': 1,
-             'neptune': 1}
 
 distances_value = {}
-distances_direction = {}
-phi_deg = {}
-d_theta = {}
-d_phi = {}
 speed_at_perihelion = {}
 omega = {}
 
 for body in bodies_list:
-    phi_deg[body] = 0
-    d_theta[body] = 0
-    d_phi[body] = 0
-    distances_direction[body] = [1, 1, 1]
     distances_value[body] = [0 * u.km, perihelion[body], 0 * u.km]
-    speed_at_perihelion[body] = np.sqrt(grav_constant * mass['sun'] / perihelion[body])
     if body != 'sun':
+        speed_at_perihelion[body] = np.sqrt(grav_constant * mass['sun'] / perihelion[body])
         omega[body] = (u.rad * speed_at_perihelion[body]) / perihelion[body]
 
 '''
-Spherical Differentiation Objects
+Spherical Differentiation Objects and Body Objects
 '''
 
 # args(name=str, mass=kg, R(radius)=units, differential=coordinates of body,
 #       a=spin factor, q=charge, is_attractor=True/False, parent=who's the parent body)
 
 sd_obj = {}
+body_obj = {}
 for object in bodies_list:
     if object != 'sun':
         sd_obj[object] = SphericalDifferential(perihelion[object],
@@ -72,26 +61,19 @@ for object in bodies_list:
                                                0 * u.km / u.s,
                                                0 * u.rad / u.s,
                                                omega[object])
-
-'''
-Body Objects
-'''
-body_obj = {}
-for object in bodies_list:
-    if object == 'sun':
+        body_obj[object] = Body(name=object, mass=mass[object], differential=sd_obj[object], parent=body_obj['sun'])
+    elif object == 'sun':
         body_obj[object] = Body(name="sun", mass=mass[object], parent=None)
     elif object == 'moon':
         body_obj[object] = Body(name=object, mass=mass[object], differential=sd_obj[object], parent=body_obj['earth'])
-    else:
-        body_obj[object] = Body(name=object, mass=mass[object], differential=sd_obj[object], parent=body_obj['sun'])
 
 '''
 Calculating Trajectory
 '''
 # Set for a year (in seconds)
-end_lambda = ((1 * u.year).to(u.s)).value
+end_lambda = {'earth': ((1 * u.year).to(u.s)).value, 'moon': ((1/12 * u.year).to(u.s)).value}
 # Choosing stepsize for ODE solver to be 5 minutes
-stepsize = ((5 * u.min).to(u.s)).value
+stepsize = {'earth': ((5 * u.min).to(u.s)).value, 'moon': ((5/12 * u.min).to(u.s)).value}
 
 '''
 Trajectory to Cartesian
@@ -105,9 +87,11 @@ def get_schwarz():
         if object != 'sun':
             schwarz_obj[object] = Schwarzschild.from_coords(sd_obj[object], mass['sun'])
             schwarz_ans[object] = schwarz_obj[object].calculate_trajectory(
-                end_lambda=end_lambda, OdeMethodKwargs={"stepsize": stepsize}, return_cartesian=True
+                end_lambda=end_lambda['earth'], OdeMethodKwargs={"stepsize": stepsize['earth']}, return_cartesian=True
             )
     return schwarz_obj, schwarz_ans
+
+
 '''
 Calculating Distance
 '''
@@ -132,7 +116,11 @@ geodesic = {}
 plotter = {}
 for object in bodies_list:
     if object != 'sun':
-        geodesic[object] = Geodesic(body=body_obj[object], time=0 * u.s, end_lambda=end_lambda, step_size=stepsize)
+        if object == 'moon':
+            geodesic[object] = Geodesic(body=body_obj[object], time=0 * u.s, end_lambda=end_lambda['moon'],
+                                        step_size=stepsize['moon'])
+        geodesic[object] = Geodesic(body=body_obj[object], time=0 * u.s, end_lambda=end_lambda['earth'],
+                                    step_size=stepsize['earth'])
 
 
 sgp = StaticGeodesicPlotter()
